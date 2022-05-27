997fb5aab400027788a6bb91dcd625865fb9b9c2
LivKrogshede <74859654+LivKrogshede@users.noreply.github.com>
2022-05-27 13:41:34 +0200

    js added

---
+++ js/faq.js
@@ -0,0 +1,56 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', function handleClick() {
+    console.log("wtf");
+    btn.innerHTML = 'Button clicked';
+  });
+
+//får info om køer frem
+function cowsFaq() {
+    var hens = document.getElementById("hens");
+    var cows = document.getElementById("cows");
+    var ducks = document.getElementById("ducks");
+    var pigs = document.getElementById("pigs");
+    pigs.classList.add("hidden");
+    hens.classList.add("hidden");
+    ducks.classList.add("hidden");
+    cows.classList.remove("hidden");
+    cows.classList.add("content-faq");
+}
+
+//får info om høns frem
+
+function hensFaq() {
+    var hens = document.getElementById("hens");
+    var cows = document.getElementById("cows");
+    var ducks = document.getElementById("ducks");
+    hens.classList.remove("hidden");
+    hens.classList.add("content-faq");
+    ducks.classList.add("hidden");
+    cows.classList.add("hidden");
+    pigs.classList.add("hidden");
+}
+
+function pigsFaq() {
+    var hens = document.getElementById("hens");
+    var cows = document.getElementById("cows");
+    var pigs = document.getElementById("pigs");
+    var ducks = document.getElementById("ducks");
+    hens.classList.add("hidden");
+    cows.classList.add("hidden");
+    ducks.classList.add("hidden");
+    pigs.classList.add("content-faq");
+    pigs.classList.remove("hidden");    
+}
+
+function ducksFaq() {
+    var hens = document.getElementById("hens");
+    var cows = document.getElementById("cows");
+    var pigs = document.getElementById("pigs");
+    var ducks = document.getElementById("ducks");
+    hens.classList.add("hidden");
+    cows.classList.add("hidden");
+    pigs.classList.add("hidden");
+    ducks.classList.remove("hidden");
+    ducks.classList.add("content-faq");    
+}
