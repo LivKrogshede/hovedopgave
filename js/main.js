2c533bc4d4d2fa63d971470c607da877e286bcd2
LivKrogshede <74859654+LivKrogshede@users.noreply.github.com>
2022-06-02 18:06:37 +0200

    templating

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+//namespace objekt  
+var main = window.main || {};
+main.loadpage = function(page) {
+    //$ = jquery som er en funktion, der er hovedfunktion i jquery
+    //funktionen returnere et objekt som også hedder jquery
+    //parametret er en css selektor som peger på content div element
+    $("#content").load(page);
+};
+
+main.init = function() {
+    window.main.loadpage("faq.html");
+
+};
+
+
+
+window.main = main;
